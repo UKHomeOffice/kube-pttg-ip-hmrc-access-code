5e6193607dcab09a478e9ae935376b957fb2869b
Tim-Hammonds <tim.hammonds@digital.homeoffice.gov.uk>
2019-09-03 08:50:29 +0100

    EE-21427: Use either prod or non-prod Kube token (#22)

--- deploy.sh
+++ deploy.sh
@@ -7,25 +7,15 @@ if [[ -z ${VERSION} ]] ; then
     export VERSION=${IMAGE_VERSION}
 fi
 
+echo "deploy ${VERSION} to ${ENVIRONMENT} namespace - using Kube token stored as drone secret"
+
 if [[ ${ENVIRONMENT} == "pr" ]] ; then
-    echo "deploy ${VERSION} to pr namespace, using PTTG_IP_PR drone secret"
     export KUBE_TOKEN=${PTTG_IP_PR}
 else
-    if [[ ${ENVIRONMENT} == "test" ]] ; then
-        echo "deploy ${VERSION} to test namespace, using PTTG_IP_TEST drone secret"
-        export KUBE_TOKEN=${PTTG_IP_TEST}
-    else
-        echo "deploy ${VERSION} to dev namespace, using PTTG_IP_DEV drone secret"
-        export KUBE_TOKEN=${PTTG_IP_DEV}
-    fi
+    export KUBE_TOKEN=${PTTG_IP_DEV}
 fi
 
-if [[ -z ${KUBE_TOKEN} ]] ; then
-    echo "Failed to find a value for KUBE_TOKEN - exiting"
-    exit -1
-fi
-
-cd kd
+cd kd || exit
 
 kd --insecure-skip-tls-verify \
     -f networkPolicy.yaml \
