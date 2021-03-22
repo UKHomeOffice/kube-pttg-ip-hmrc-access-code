8e7a70709bf6ce73114a3a343e1ac8216dc46146
Tim-Hammonds <tim.hammonds@digital.homeoffice.gov.uk>
2021-03-22 11:31:11 +0000

    EE-34375: Determine which env var to use for image version (#29)

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,16 @@ export KUBE_NAMESPACE=${KUBE_NAMESPACE}
 export KUBE_SERVER=${KUBE_SERVER}
 export DEPLOYMENT_NAME=${DEPLOYMENT_NAME:-pttg-ip-hmrc-access-code}
 
-if [[ -z ${VERSION} ]] ; then
+echo
+echo "The whole env of deploy.sh is:"
+env
+echo
+echo "====================================="
+echo
+
+if [[ -z ${IMAGE_VERSION} ]] ; then
+    export VERSION=${DRONE_BUILD_NUMBER}
+else
     export VERSION=${IMAGE_VERSION}
 fi
 
